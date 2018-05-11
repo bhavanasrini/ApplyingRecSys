b7127e4a2fc3501adb4b47fda4713f426947fa45
Bhavana Srinivasan <bhavanaaiyer@gmail.com>
2018-05-11 13:51:11 -0600

    First phase of CB

---
+++ contentBasedApproach/indexing/mapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import os
+count = 0
+for line  in sys.stdin.readlines():
+	#next(line)
+	arr = line.strip().split(",")
+	if(count == 0):
+		count = 1 
+		continue
+	print(("LatLong",(arr[1],(arr[9],arr[10]))))
+	print(("orgid",(arr[1],arr[4])))
+	print(("age",(arr[1],(arr[5],arr[6]))))
+	print(("startdate",(arr[1],arr[8])))
---
+++ contentBasedApproach/indexing/reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+import os
+import ast
+
+key_dict = {}
+for line  in sys.stdin:
+	key_line = (ast.literal_eval(line))
+	if key_line[0] in key_dict.keys():
+		temp = key_dict[key_line[0]]
+		if key_line[1][1] in temp.keys():
+			val = temp[key_line[1][1]]
+			val.append(key_line[1][0])
+			temp[key_line[1][1]] = val
+		else:
+			val = [key_line[1][0]]
+			temp[key_line[1][1]] = val
+	else:
+		val  = {}
+		val[key_line[1][1]] = [key_line[1][0]]
+		key_dict[key_line[0]] = val
+for i in key_dict.keys():
+	print((str(i),key_dict[i]))
