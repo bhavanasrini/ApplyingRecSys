1c6bb03c2d97b6a7faa7ebcbcf2d336e65e93dce
Bhavana Srinivasan <bhavanaaiyer@gmail.com>
2018-05-11 14:03:10 -0600

    Second phase of CF

---
+++ CollaborativeFiltering/mapredphase2/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+import os
+import numpy as np
+import ast
+item_dict = {}
+for lines in sys.stdin:
+	item_dict = ast.literal_eval(lines)
+
+items = list(item_dict.keys())
+for i in range(len(items)):
+	item1 = items[i]
+	list1 = item_dict[item1]
+	for j in range(i,len(items)):
+		item2 = items[j]
+		list2 = item_dict[item2]
+		intersectLen = len(np.intersect1d(list1,list2))
+		unionLen = len(np.union1d(list1,list2))
+		print(((item1,item2),float(intersectLen/unionLen)))
+		print(((item2,item1),float(intersectLen/unionLen)))
+
